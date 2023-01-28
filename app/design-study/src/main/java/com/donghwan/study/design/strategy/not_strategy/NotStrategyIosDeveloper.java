c2073c3c8c2d5a92e009212fc973bfb1c368aba9
dev-donghwan <ycdoit@naver.com>
2023-01-29 01:08:03 +0900

    feat: add strategy pattern

---
+++ app/design-study/src/main/java/com/donghwan/study/design/strategy/not_strategy/NotStrategyIosDeveloper.java
@@ -0,0 +1,9 @@
+package com.donghwan.study.design.strategy.not_strategy;
+
+public class NotStrategyIosDeveloper implements NotStrategyDeveloper {
+
+    @Override
+    public void coding() {
+        System.out.println("coding with ObjectC...");
+    }
+}
---
+++ app/design-study/src/main/java/com/donghwan/study/design/strategy/strategy/StrategyAndroidDeveloper.java
@@ -0,0 +1,10 @@
+package com.donghwan.study.design.strategy.strategy;
+
+import com.donghwan.study.design.strategy.strategy.strategy.CodingBehavior;
+
+public class StrategyAndroidDeveloper extends StrategyDeveloper {
+
+    public StrategyAndroidDeveloper(CodingBehavior codingBehavior) {
+        super(codingBehavior);
+    }
+}
