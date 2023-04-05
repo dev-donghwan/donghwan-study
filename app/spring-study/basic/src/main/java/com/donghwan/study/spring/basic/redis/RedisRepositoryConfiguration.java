091954a6eddb7dc8631aa9ca89b59060528ba743
dev-donghwan <ycdoit@naver.com>
2023-04-05 18:24:44 +0900

    feat: change value path

--- app/spring-study/basic/src/main/java/com/donghwan/study/spring/basic/redis/RedisRepositoryConfiguration.java
+++ app/spring-study/basic/src/main/java/com/donghwan/study/spring/basic/redis/RedisRepositoryConfiguration.java
@@ -14,10 +14,10 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @Configuration
 public class RedisRepositoryConfiguration {
 
-    @Value("${spring.cache.redis.host}")
+    @Value("${spring.redis.host}")
     private String host;
 
-    @Value("${spring.cache.redis.port}")
+    @Value("${spring.redis.port}")
     private int port;
 
     @Bean
