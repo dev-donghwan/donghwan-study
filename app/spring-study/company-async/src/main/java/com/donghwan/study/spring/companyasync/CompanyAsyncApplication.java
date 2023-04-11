4f33a26e0bfb274045d70bdeda9a7d21dbeeabca
pc <pc@pcui-MacBookPro.local>
2023-04-10 23:20:29 -0700

    refactor: add new module

---
+++ app/spring-study/company-async/src/main/java/com/donghwan/study/spring/companyasync/CompanyAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.donghwan.study.spring.companyasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyAsyncApplication.class, args);
+	}
+
+}
---
+++ app/spring-study/company-async/src/test/java/com/donghwan/study/spring/companyasync/CompanyAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donghwan.study.spring.companyasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
